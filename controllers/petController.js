c86e3d7de5e4691229ebb18a902ba3e7efd1be03
chris-plonka <info@krzysztofplonka.pl>
2024-05-02 06:29:08 +0200

    End off day progress for contact form feautere

--- controllers/petController.js
+++ controllers/petController.js
@@ -74,14 +74,6 @@ exports.actuallyUpdatePet = async (req, res) => {
     res.redirect("/")
   }
 
-  if (typeof req.body.name != "string") {
-    req.body.name = ""
-  }
-  
-  if (typeof req.body.description != "string") {
-    req.body.description = ""
-  }
-
   let ourNewObject = { name: sanitizeHtml(req.body.name, sanitizeOptions), birthYear: new Date().getFullYear(), species: sanitizeHtml(req.body.species, sanitizeOptions), description: sanitizeHtml(req.body.description, sanitizeOptions) }
 
   if (req.body.birthYear > 999 && req.body.birthYear < 9999) {
@@ -109,14 +101,6 @@ exports.actuallyUpdatePet = async (req, res) => {
 }
 
 exports.storePet = async (req, res) => {
-  if (typeof req.body.name != "string") {
-    req.body.name = ""
-  }
-  
-  if (typeof req.body.description != "string") {
-    req.body.description = ""
-  }
-  
   let ourObject = { name: sanitizeHtml(req.body.name, sanitizeOptions), birthYear: new Date().getFullYear(), species: sanitizeHtml(req.body.species, sanitizeOptions), description: sanitizeHtml(req.body.description, sanitizeOptions) }
 
   if (req.body.birthYear > 999 && req.body.birthYear < 9999) {
